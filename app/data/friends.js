aa885ca21621720d067dc970fb2d8cc7e6769729
Emma Weber <emma.weber327@gmail.com>
2019-10-11 17:00:50 -0600

    added the necessary files and folders. Built out the requires and bones for all JS files and installed NPMs.

---
+++ app/data/friends.js
@@ -0,0 +1,21 @@
+var friends = [
+    {
+        name: "Joel",
+        photo: "https://pbs.twimg.com/profile_images/1002666089640153089/faKAzfEo.jpg",
+        scores: [
+            5,
+            3,
+            1,
+            4,
+            3,
+            5,
+            2,
+            1,
+            4,
+            5
+        ]
+
+    }
+]
+
+module.exports = friends
---
+++ server.js
@@ -0,0 +1,14 @@
+var express = require("express");
+
+var app = express();
+var PORT = process.env.PORT || 8080;
+
+app.use(express.urlencoded({extended: true}));
+app.use(express.json());
+
+require("./app/routing/apiRoutes");
+require("./app/routing/htmlRoutes");
+
+app.listen(PORT, function() {
+    console.log("App listening on PORT " + PORT);
+  });
