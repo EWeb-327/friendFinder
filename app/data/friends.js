fd912733999b4c0e470655c5e074b4236d4ae0d0
Emma Weber <emma.weber327@gmail.com>
2019-10-12 14:27:59 -0600

    added initial array of friends with names, photos and scores

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,6 +1,6 @@
 var friends = [
     {
-        name: "Joel",
+        name: "Joel A.",
         photo: "https://pbs.twimg.com/profile_images/1002666089640153089/faKAzfEo.jpg",
         scores: [
             5,
@@ -17,7 +17,7 @@ var friends = [
 
     },
     {
-        name: "Elizabeth",
+        name: "Elizabeth B.",
         photo: "https://dglawpa.com/wp-content/uploads/DSC_8647-684x1024.jpg",
         scores: [
             4,
@@ -30,6 +30,134 @@ var friends = [
             3,
             2,
             4
+        ]
+    },
+    {
+        name: "Tom C.",
+        photo: "https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/actor-tom-holland-looks-on-during-a-practice-round-ahead-of-news-photo-1079651980-1561474294.jpg?resize=480:*",
+        scores: [
+            3,
+            5,
+            1,
+            4,
+            2,
+            4,
+            5,
+            5,
+            1,
+            3
+        ]
+    },
+    {
+        name: "Susan D.",
+        photo: "https://www.sns.ias.edu/~seclark/images/susan.jpg",
+        scores: [
+            1,
+            2,
+            3,
+            4,
+            5,
+            1,
+            2,
+            3,
+            4,
+            5
+        ]
+    },
+    {
+        name: "Olivia E.",
+        photo: "https://pbs.twimg.com/profile_images/1036393341787811840/e7zwilw5_400x400.jpg",
+        scores: [
+            2,
+            3,
+            4,
+            5,
+            1,
+            2,
+            3,
+            4,
+            5,
+            1
+        ]
+    },
+    {
+        name: "Ricky F.",
+        photo: "https://images.spot.im/v1/production/lcjgvtr3scrrhyjpxdix",
+        scores: [
+            2,
+            2,
+            4,
+            4,
+            1,
+            1,
+            3,
+            3,
+            5,
+            5
+        ]
+    },
+    {
+        name: "Melissa G.",
+        photo:"https://cleanmyspace.com/wp-content/uploads/2018/04/about-melissa-maker.jpg",
+        scores: [
+            5,
+            4,
+            3,
+            2,
+            1,
+            5,
+            4,
+            3,
+            2,
+            1
+        ]
+    },
+    {
+        name: "Joe H.",
+        photo:"https://pbs.twimg.com/profile_images/1110248536312938496/3a8AhknL.jpg",
+        scores: [
+            1,
+            3,
+            5,
+            5,
+            4,
+            2,
+            1,
+            5,
+            4,
+            4
+        ]
+    },
+    {
+        name: "Layne I.",
+        photo: "https://cascadeenergy.com/wp-content/uploads/2018/01/layne-mcwilliams.jpg",
+        scores: [
+            2,
+            5,
+            1,
+            1,
+            3,
+            4,
+            5,
+            4,
+            2,
+            1
+        ]
+    },
+    {
+        name: "Tasha J.",
+        photo: "http://oneteammarketing.com/wp-content/uploads/2018/03/Tasha-Peterson-e1522365576559.jpg",
+        scores: [
+            5,
+            5,
+            5,
+            5,
+            5,
+            5,
+            5,
+            5,
+            5,
+            5
         ]
     }
 ]
